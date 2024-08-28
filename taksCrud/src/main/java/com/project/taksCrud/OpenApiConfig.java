52913ae8528ca0e4a4cb95e6ac90ed3f73186099
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-08-28 11:03:18 -0300

    configuration of project

---
+++ taksCrud/src/main/java/com/project/taksCrud/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.project.taksCrud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI tasksCrudOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Tasks Crud API")
+                .description("API  com CRUD de tasks")
+                .version("1.0"));
+    }
+}
