7315c081311460945cfe15846c0452593de14a26
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-08-28 10:36:29 -0300

    create project

---
+++ taksCrud/src/main/java/com/project/taksCrud/TaksCrudApplication.java
@@ -0,0 +1,13 @@
+package com.project.taksCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaksCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaksCrudApplication.class, args);
+	}
+
+}
