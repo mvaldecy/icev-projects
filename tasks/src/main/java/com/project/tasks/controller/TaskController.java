fc1eebc5174ed4c28cb940aca614f80c3f9dab11
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-08-30 16:43:13 -0300

    remove unused imports

--- tasks/src/main/java/com/project/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/project/tasks/controller/TaskController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
--- tasks/src/main/java/com/project/tasks/services/TaskService.java
+++ tasks/src/main/java/com/project/tasks/services/TaskService.java
@@ -1,13 +1,10 @@
 package com.project.tasks.services;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
