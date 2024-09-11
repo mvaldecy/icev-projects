8032dae35a5dd0039695480a1eb5f4f896168181
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-09-11 08:31:29 -0300

    Delete agendamento directory

--- agendamento/src/main/java/com/reserva/agendamento/repositories/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reserva.agendamento.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.reserva.agendamento.entities.Reserva;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long>{
-    
-}
